daf3eb28fa3b811242ab44a5a25294d61ee79302
Snorone <tobias.hurtig@gritacademy.se>
2024-02-16 11:23:22 +0100

    testing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import karate from "./media/karate-fighter-silhouette-.jpg";
+import anime from "animejs";
 
 const img = document.createElement("img");
 document.body.append(img);
 img.src = karate;
+
+anime({
+  targets: img,
+  rotate: 360,
+});
